b2df0b168e6c5ba9c4e2a48c5c175988a59a1935
Ben Dellarocco <bendellarocco@gmail.com>
2015-07-06 15:09:10 -0400

    Falling Object Support

---
+++ Assets/Resources/Scripts/fallingObject.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class fallingObject : MonoBehaviour {
+
+	public float fallSpeed;
+	Rigidbody2D target;
+
+	void Start () {
+		target = this.GetComponent<Rigidbody2D>();
+	}
+	
+
+	void Update () {
+		RaycastHit2D hit = Physics2D.Raycast (this.transform.position, -Vector2.up);
+		if (hit.collider.tag == "Player") {
+			target.isKinematic = false;
+			target.gravityScale = fallSpeed;
+
+		}
+
+	}
+}
