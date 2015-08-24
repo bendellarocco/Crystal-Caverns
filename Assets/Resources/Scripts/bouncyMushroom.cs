74385f2dcc4b4340f37dbfdf081b083348c1e89a
Ben Dellarocco <bendellarocco@gmail.com>
2015-08-24 17:07:08 -0400

    Closes #36 Bouncy Mushroom Tech Now In and Working
    Bouncy Mushroom Tech Now In and Working

---
+++ Assets/Resources/Scripts/bouncyMushroom.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class bouncyMushroom : MonoBehaviour {
+
+	public float bounce;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnCollisionEnter2D(Collision2D hit) {
+		if (hit.collider.tag == "Player") {
+			//hit.rigidbody.AddForce(new Vector2(-hit.relativeVelocity.x, hit.relativeVelocity.y) * bounce, ForceMode2D.Impulse);
+			hit.rigidbody.AddForce(Vector2.up * bounce, ForceMode2D.Impulse);
+		}
+
+	}
+}
