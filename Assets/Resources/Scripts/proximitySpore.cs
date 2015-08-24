13c510aef94cee4cd03f97b482d07f21345e88cf
Ben Dellarocco <bendellarocco@gmail.com>
2015-08-24 17:43:49 -0400

    Made the spores more user friendly for testing
    They now bink and explode after a set time

--- Assets/Resources/Scripts/proximitySpore.cs
+++ Assets/Resources/Scripts/proximitySpore.cs
@@ -5,9 +5,17 @@ public class proximitySpore : MonoBehaviour {
 	
 	public float range;
 	float distance;
+	public float timer;
+	float countdown;
+
+	//TEMP BLINK STUFF
+	float delay = 0.25f;
+	Renderer rend;
 
 	// Use this for initialization
 	void Start () {
+		countdown = timer;
+		rend = this.GetComponent<Renderer>();
 	}
 	
 	// Update is called once per frame
@@ -16,8 +24,28 @@ public class proximitySpore : MonoBehaviour {
 		distance = Vector3.Distance (player.myTrans.position, this.transform.position);
 
 		if (distance <= range) {
-			Debug.Log ("TOO CLOSE");
+			if (countdown <= 0) {
+				explode ();
+			} else {
+				countdown -= Time.deltaTime;
+				rend.enabled = false;
+				Invoke("show", delay);
+
+			}
+
+		} else {
+			countdown = timer;
 		}
 	
 	}
+
+
+	void show() {
+		rend.enabled = true;
+	}
+
+	void explode ()
+	{
+		Application.LoadLevel (Application.loadedLevel);
+	}	
 }
